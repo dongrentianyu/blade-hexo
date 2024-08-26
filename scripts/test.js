b5abe0c27eb797e6d4e584bfa59cb4be982ba38d
dongrentianyu <3465645884@qq.com>
2024-08-26 21:46:36 +0800

    test

---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+// hexo.extend.injector.register(
+//   "body_end",
+//   '<script src="/test.js"></script>',
+//   "default"
+// );
+
+// hexo.extend.filter.register("theme_inject", function (injects) {
+//   injects.postMarkdownEnd.raw(
+//     "post",
+//     '<div style="text-align: center; white-space: pre-line;">00023669</div>'
+//   );
+//   //   injects.bodyEnd.raw("post", ");
+// });
